ab8b4942ca75128d99be3e9b21e7d95f5ff7b407
MeanOfRedStone <hongs7701@naver.com>
2024-06-27 17:42:27 +0900

    test : userMapper의 findUser의 정상 작동 확인
    1. given : id, password, (이하 생략...)가 'admin' 'admin' (이하 생략...) 인 데이터를 db에 추가
    
    2. when : id, password가 'admin' 'admin'인 login인 정보를 입력하면
    
    3. user_id, id, name을 가진 user 객체를 반환

--- CRUDPractice/src/test/java/com/springboot/crudpractice/mapper/UserMapperTest.java
+++ CRUDPractice/src/test/java/com/springboot/crudpractice/mapper/UserMapperTest.java
@@ -4,10 +4,10 @@ import com.springboot.crudpractice.user.domain.User;
 import org.apache.ibatis.session.SqlSession;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -19,7 +19,6 @@ public class UserMapperTest {
     private SqlSession sqlSession;
 
     @Test
-    @Rollback
     void testSaveUser() {
         User testUser = User.builder().id("admin").password("admin").name("admin").email("admin").phone("999").address("seoul")
                 .addressDetail("Jongno").agreement(1).phoneContact(1).emailContact(1).build();
@@ -28,11 +27,23 @@ public class UserMapperTest {
 
         sqlSession.insert("UserMapper.saveUser", testUser);
         int actualCount = countUsers();
-
         assertEquals(expectedCount, actualCount);
     }
 
     private int countUsers() {
         return sqlSession.selectOne("UserMapper.countUsers");
     }
+
+    @Test
+    void testFindUser() {
+        User adminUser = User.builder().id("admin").password("admin").name("admin").email("admin").phone("999").address("seoul")
+                .addressDetail("Jongno").agreement(1).phoneContact(1).emailContact(1).build();
+        sqlSession.insert("UserMapper.saveUser", adminUser);
+
+        User loginUser = User.builder().id("admin").password("admin").build();
+        User actualUser = sqlSession.selectOne("UserMapper.findUser", loginUser);
+
+        assertNotNull(actualUser);
+    }
+
 }
