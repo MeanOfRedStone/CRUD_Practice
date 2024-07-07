2208cd31643a2309913840a095e567e07870af0b
MeanOfRedStone <hongs7701@naver.com>
2024-07-08 00:01:56 +0900

    feat : ProductRegistrationDto 생성

---
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductRegistrationDto.java
@@ -0,0 +1,15 @@
+package com.springboot.crudpractice.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProductRegistrationDto {
+    private String name;
+    private int price;
+    private String category;
+    private String image;
+    private String information;
+    private String measurment;
+}
