c3773d30d86f27d189de34b50d7d711e358f318f
MeanOfRedStone <hongs7701@naver.com>
2024-07-07 22:59:04 +0900

    feat : join requestDto 생성
    1. Builder는 클래스 위에만 붙인다
    
    2. dto 객체 생성시 객체 불변성을 위해 setter, noargsconstructor 등을 일반적으로 사용하지 않는다.
    
    3. dto에서는 클래스 메소드를 만들지 않는다

---
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/user/dto/JoinRequestDto.java
@@ -0,0 +1,18 @@
+package com.springboot.crudpractice.user.dto;
+
+import lombok.*;
+
+@Builder
+@Getter
+public class JoinRequestDto {
+    private String id;
+    private String password;
+    private String name;
+    private String phone;
+    private String email;
+    private String address;
+    private String addressDetail;
+    private int agreement;
+    private int phoneContact;
+    private int emailContact;
+}
