ef246ab1fd1ff5005c2375865fddefa4e9060fb3
MeanOfRedStone <hongs7701@naver.com>
2024-07-10 19:01:07 +0900

    feat : ProductDetailDto 생성

---
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductDetailDto.java
@@ -0,0 +1,19 @@
+package com.springboot.crudpractice.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProductDetailDto {
+    private long itemId;
+    private String name;
+    private int price;
+    private String category;
+    private String image;
+    private String information;
+    private String measurment;
+    private long detailId;
+    private String option;
+    private int quantity;
+}
