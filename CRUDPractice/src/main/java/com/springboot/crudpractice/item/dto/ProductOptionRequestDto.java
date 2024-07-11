c743cbe41f0eb057722c6270345ab2362ecf9b36
MeanOfRedStone <hongs7701@naver.com>
2024-07-11 16:03:44 +0900

    fix : ProductOptionRequestDto 필드 추가
    detailId 추가

--- CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionRequestDto.java
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionRequestDto.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 @Getter
 @Builder
 public class ProductOptionRequestDto {
+    private long detailId;
     private long itemId;
     private String option;
     private int quantity;
