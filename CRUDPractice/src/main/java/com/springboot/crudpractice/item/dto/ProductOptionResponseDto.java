4bca395e70322e3e7c9baace6e7846fe397a0adb
MeanOfRedStone <hongs7701@naver.com>
2024-07-11 17:04:53 +0900

    feat : ProductOptionDto 객체 재 생성
    단일 itemDetail 정보만 가지는 Dto로 역할 변경

--- CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionResponseDto.java
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionResponseDto.java
@@ -1,16 +1,13 @@
 package com.springboot.crudpractice.item.dto;
 
-import com.springboot.crudpractice.item.domain.ItemDetail;
+import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Collections;
-import java.util.List;
-
 @Getter
+@Builder
 public class ProductOptionResponseDto {
-    private List<ItemDetail> itemDetails;
-
-    public ProductOptionResponseDto(List<ItemDetail> itemDetails) {
-        this.itemDetails = Collections.unmodifiableList(itemDetails);
-    }
+    private long detailId;
+    private long itemId;
+    private String option;
+    private int quantity;
 }
