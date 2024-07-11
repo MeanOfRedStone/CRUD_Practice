944f5ea18559184f603054a9adc9202fb99f3c47
MeanOfRedStone <hongs7701@naver.com>
2024-07-11 15:58:39 +0900

    feat : ProductOptionRequestDto 생성

---
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionRequestDto.java
@@ -0,0 +1,12 @@
+package com.springboot.crudpractice.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ProductOptionRequestDto {
+    private long itemId;
+    private String option;
+    private int quantity;
+}
