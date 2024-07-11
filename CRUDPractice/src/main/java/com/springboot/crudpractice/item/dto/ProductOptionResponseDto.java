f7bae34194d32711af638313d45d49763abf946f
MeanOfRedStone <hongs7701@naver.com>
2024-07-11 16:35:47 +0900

    fix : lombok Getter 어노테이션 추가

--- CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionResponseDto.java
+++ CRUDPractice/src/main/java/com/springboot/crudpractice/item/dto/ProductOptionResponseDto.java
@@ -1,10 +1,12 @@
 package com.springboot.crudpractice.item.dto;
 
 import com.springboot.crudpractice.item.domain.ItemDetail;
+import lombok.Getter;
 
 import java.util.Collections;
 import java.util.List;
 
+@Getter
 public class ProductOptionResponseDto {
     private List<ItemDetail> itemDetails;
 
